2dd5d73201d62dc54ec42290cb2bdf36b46fa481
unknown <valododa123@gmail.com>
2025-08-02 12:42:23 +0200

    Description Collapse component

---
+++ src/components/DescriptionCollapse.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="description_collapse">
+    <p :class="{ 'truncate-3-lines': !isActive }"><slot></slot></p>
+    <button :text="text" @click="open">{{text}}</button>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+let text = ref('shiko më shumë');
+let isActive = ref(false);
+
+function open() {
+    isActive.value = !isActive.value
+    isActive.value ? text.value = "shiko më pak" : text.value = "shiko më shumë" 
+}
+</script>
+<style scoped>
+button {
+  background-color: rgba(1,1,1,0);
+  border: none;
+  cursor: pointer;
+  color: var(--paragraph-color);
+  font-family: var(--paragraph-font);
+  font-size: 0.9rem;
+  text-decoration: underline;
+}
+.description_collapse p {
+  font-size: 1rem;
+}
+</style>
